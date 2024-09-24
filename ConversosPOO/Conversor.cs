415aa8f0fe8fd858d19733c4966c64b5ae093c7f
angellicalist <madu.grassi@gmail.com>
2024-09-24 08:21:38 -0300

    Add files via upload

---
+++ ConversosPOO/Conversor.cs
@@ -0,0 +1,12 @@
+public class Conversor
+{
+    public static double CelsiusParaFahrenheit(double celsius)
+    {
+        return (celsius * 9 / 5 ) + 32;
+    }
+
+    public static double FahrenheitParaCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
---
+++ ConversosPOO/Program.cs
@@ -0,0 +1,3 @@
+﻿double celsius = 25.0;
+double fahrenheit = Conversor.CelsiusParaFahrenheit(celsius);
+Console.WriteLine($"{celsius}°C é igual a {fahrenheit}°F");
