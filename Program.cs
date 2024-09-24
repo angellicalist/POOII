45ce6f160f7fa786a10ddba33bd1fe7aff50ab9a
angellicalist <madu.grassi@gmail.com>
2024-09-24 08:28:34 -0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Veiculo carro = new Carro ("Lamborghini ", "Huracan STO", 2);
-Veiculo moto = new Moto ("Honda", "PCX 160 DLX", false);
-
-carro.ExibirDetalhes();
-carro.TipoDeVeiculo();
-
-moto.ExibirDetalhes();
-moto.TipoDeVeiculo();
